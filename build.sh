c9ab63228dc4452213c22ba415bc9fe61dd8324c
zonble <zonble@gmail.com>
2019-10-22 15:32:08 +0800

    Updates build settings.

--- build.sh
+++ build.sh
@@ -1,11 +1,12 @@
 rm -Rf build
 
-xcodebuild -project SwiftOpenCC/SwiftyOpenCC.xcodeproj -target OpenCCBridge -sdk iphoneos -arch armv7 -arch armv7s -arch arm64 -arch arm64e  SYMROOT="../build" OTHER_CFLAGS="-fembed-bitcode" BITCODE_GENERATION_MODE="bitcode"
-xcodebuild -project SwiftOpenCC/SwiftyOpenCC.xcodeproj -target OpenCCBridge -sdk iphonesimulator -arch i386 -arch x86_64 SYMROOT="../build" OTHER_CFLAGS="-fembed-bitcode" BITCODE_GENERATION_MODE="bitcode"
+xcodebuild -project SwiftOpenCC/SwiftyOpenCC.xcodeproj -target OpenCCBridge -sdk iphoneos -arch armv7 -arch armv7s -arch arm64 -arch arm64e  SYMROOT="../build" OTHER_CFLAGS="-fembed-bitcode" BITCODE_GENERATION_MODE=bitcode ONLY_ACTIVE_ARCH=NO 2>&1
+xcodebuild -project SwiftOpenCC/SwiftyOpenCC.xcodeproj -target OpenCCBridge -sdk iphonesimulator -arch i386 -arch x86_64 SYMROOT="../build" OTHER_CFLAGS="-fembed-bitcode" BITCODE_GENERATION_MODE=bitcode ONLY_ACTIVE_ARCH=NO 2>&1
 
 rm -rf build/Release-universal
 mkdir build/Release-universal
 mkdir build/Release-universal/OpenCCBridge.framework
+
 cp build/Release-iphoneos/OpenCCBridge.framework/Info.plist build/Release-universal/OpenCCBridge.framework/.
 lipo -create build/Release-iphoneos/OpenCCBridge.framework/OpenCCBridge build/Release-iphonesimulator/OpenCCBridge.framework/OpenCCBridge -output build/Release-universal/OpenCCBridge.framework/OpenCCBridge
 rm -rf lib
